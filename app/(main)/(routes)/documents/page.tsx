89306cacaafdc4d4b5605ec795ac5102f203fe84
Dmitriifd <idimka9@gmail.com>
2023-11-02 16:13:44 +0500

    Sidebar

---
+++ app/(main)/(routes)/documents/page.tsx
@@ -0,0 +1,11 @@
+
+const DocumentsPage = () => {
+
+  return (
+    <div className="h-full flex flex-col items-center justify-center space-y-4">
+      Document page
+    </div>
+   );
+}
+
+export default DocumentsPage;
